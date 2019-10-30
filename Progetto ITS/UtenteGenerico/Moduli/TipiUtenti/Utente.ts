1f8852d22fcdf004d73cadb4935817fe1b35ee0a
Ezio <racitiezio@gmail.com>
2019-10-30 17:07:01 +0100

    bug fix

--- Progetto ITS/UtenteGenerico/Moduli/TipiUtenti/Utente.ts
+++ Progetto ITS/UtenteGenerico/Moduli/TipiUtenti/Utente.ts
@@ -1,65 +1,67 @@
-import {Telefono, Email, CF} from "../Validazioni/validazioni";
+import { Telefono, Email, CF } from "../Validazioni/validazioni";
 
-
-
+//Interfaccia Utente
 interface Utente {
-    nome?:string;
-    cognome?:string;
-    email:Email;
-    privacy:boolean;
-    telefono?:Telefono;
-
-    
+    nome?: string;
+    cognome?: string;
+    email: Email;
+    privacy: boolean;
+    telefono?: Telefono;
 }
 
-export class UtenteGenerico implements Utente{
-    email:Email;
-    privacy:boolean= false;
 
-    constructor(email:string , privacy:boolean){
-        this.email=new Email(email);
-        this.privacy=privacy;
-       
+
+//Classe UtenteGenerico che implementa Utente
+export class UtenteGenerico implements Utente {
+    email: Email;
+    privacy: boolean = false;
+
+    constructor(email: string, privacy: boolean) {
+        this.email = new Email(email);
+        this.privacy = privacy;
     }
 }
 
-export class UtenteInteressato extends UtenteGenerico{
 
-   
-    nome:string="";
-    cognome:string="";
-    telefono:Telefono;
-    constructor(email:string ,nome:string, cognome:string, privacy:boolean, telefono:string ){
-        
-        super(email,privacy);
-        this.nome=nome;
-        this.cognome=cognome;
-        this.telefono= new Telefono(telefono);
+
+//Classe UtenteInteressato che estende UtenteGenerico
+export class UtenteInteressato extends UtenteGenerico {
+
+
+    nome: string = "";
+    cognome: string = "";
+    telefono: Telefono;
+    constructor(email: string, nome: string, cognome: string, privacy: boolean, telefono: string) {
+
+        super(email, privacy);
+        this.nome = nome;
+        this.cognome = cognome;
+        this.telefono = new Telefono(telefono);
     }
-
-}
-
-export class UtenteIscritto extends UtenteInteressato{
-
-codice_fiscale:CF;
-
-constructor(email:string ,nome:string, cognome:string, privacy:boolean, telefono:string, codice_fiscale:string ){
-    super(nome,cognome,email,privacy,telefono);
-    this.codice_fiscale=new CF(codice_fiscale);
-
-}
-
 }
 
 
-export class UtenteVoti extends UtenteIscritto{
-    voti:number;
 
-    constructor(email:string ,nome:string, cognome:string, privacy:boolean, telefono:string, codice_fiscale:string, voti:number ){
-        super(nome,cognome,email,privacy,telefono,codice_fiscale);
-        this.voti=voti;
+//Classe UteneIscritto che estende UtenteInteressato
+export class UtenteIscritto extends UtenteInteressato {
 
+    codice_fiscale: CF;
 
+    constructor(email: string, nome: string, cognome: string, privacy: boolean, telefono: string, codice_fiscale: string) {
+        super(nome, cognome, email, privacy, telefono);
+        this.codice_fiscale = new CF(codice_fiscale);
     }
- 
+}
+
+
+
+//Classe UtenteVoti che estende UtenteIscritto
+export class UtenteVoti extends UtenteIscritto {
+
+    voti: number;
+
+    constructor(email: string, nome: string, cognome: string, privacy: boolean, telefono: string, codice_fiscale: string, voti: number) {
+        super(nome, cognome, email, privacy, telefono, codice_fiscale);
+        this.voti = voti;
+    }
 }
